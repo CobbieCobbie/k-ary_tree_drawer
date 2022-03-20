5b91f34e4b3c464f1773b21636ddb0d1ae08db38
cobbiecobbie <Benjamin.Coban@gmail.com>
2022-03-20 13:26:07 +0100

    Logging and argparse module added
    Argparse:
    Used to fetch $k$ and the height for the tree. Also, "--integer" or "-i" boolean sets whether its a grid or a plane
    
    Logging:
    Log dump into ${timestamp}_${k}-ary_tree_with_height_${h}.log

--- k-ary_tree_drawer.py
+++ k-ary_tree_drawer.py
@@ -1,8 +1,9 @@
-import sys
 import math
 import networkx as nx
 import matplotlib.pyplot as plot
 import time
+import logging as log
+import argparse
 
 
 class Vertex:
@@ -16,6 +17,10 @@ class Vertex:
                "\n Height equals: " + str(self.height)
 
 
+integer_grid = False
+k = 2
+h = 3
+
 v_max = 0
 v_counter = 0
 l_max = 0
@@ -23,25 +28,41 @@ l_min = float("inf")
 
 
 def main():
-
-    # argument eval
-
-    _time = time.time()
-    k = 2
-    h = 3
-    args = sys.argv[1:]
-    if len(args) >= 2:
-        if args[0] == "-k":
-            k = int(args[1])
-        if args[0] == "-h":
-            h = int(args[1])
-    if len(args) >= 4:
-        if args[2] == "-k":
-            k = int(args[3])
-        if args[2] == "-h":
-            h = int(args[3])
+    parser = argparse.ArgumentParser(description="Draws a k-ary tree with parameters k and height h")
+    parser.add_argument("-k",
+                        nargs=1,
+                        type=int,
+                        dest="k",
+                        help="maximum amount of children for any vertex"
+                        )
+    parser.add_argument("--height",
+                        "-he",
+                        nargs=1,
+                        type=int,
+                        dest="h",
+                        help="height of the k-ary tree")
+    parser.add_argument("--integer",
+                        "-i",
+                        dest="integer",
+                        help="Places the vertices on integer grid points by rounding the coordinates")
+    args = parser.parse_args()
 
     # further initializations
+
+    global k, h, integer_grid
+    k = args.k[0]
+    h = args.h[0]
+    integer_grid = args.integer
+
+    log.info("Parameters:")
+    log.info(f"k = {k}, h = {h}, integer grid = {integer_grid}")
+
+    _time = time.time()
+    timestamp = time.gmtime()
+
+    log.basicConfig(filename=f"{timestamp.tm_year}-{timestamp.tm_mon}-{timestamp.tm_mday}_"
+                             f"{timestamp.tm_hour}h{timestamp.tm_min}m{timestamp.tm_sec}s_"
+                             f"{k}-ary_tree_with_h={h}.log", encoding='utf-8', level=log.DEBUG)
 
     global v_counter, v_max
     v_max = (pow(k, h + 1) - 1) / (k - 1)
@@ -74,14 +95,18 @@ def main():
     # print results
 
     print("########################################")
+    log.debug("l_min: " + str(l_min))
     print("l_min: " + str(l_min))
     print("l_max: " + str(l_max))
     print("Ratio of resulting drawing: " + str(l_max / l_min))
+    log.debug("l_max: " + str(l_max))
+    log.debug("Ratio of resulting drawing: " + str(l_max / l_min))
 
     _time = time.time() - _time
     _minutes = int(_time / 60)
     _seconds = _time % 60
     print(f"The process took {_minutes:.0f} minutes and {_seconds:.3f} seconds!")
+    print("integer grid: " + str(integer_grid))
     plot.show()
 
 
@@ -90,14 +115,12 @@ def draw_vertices(v, r, h, k, G):
         d = 2*pow(k, h-v.height-1)
         x_start = v.coordinates[0] - (k-1)*pow(k, h-v.height-1)
         for i in range(k):
-            # print(i)
             x_coord = x_start + i * d
-            # print("x_coord: "+str(x_coord))
-            # print("v.coordinates: " + str(v.coordinates[1]))
             y_coord = v.coordinates[1] - math.sqrt(r*r - (x_coord - v.coordinates[0])*(x_coord - v.coordinates[0]))
-            y_coord = round(y_coord, 0)
+            if integer_grid == True:
+                y_coord = round(y_coord, 0)
+                print("y rounded")
             v_child = Vertex(x_coord, y_coord, v.height+1)
-            # print(v_child)
 
             G.add_node(v_child)
             global v_counter, v_max
@@ -113,8 +136,8 @@ def draw_vertices(v, r, h, k, G):
                 l_min = edge_length
             if edge_length > l_max:
                 l_max = edge_length
-            print("\n Percentage of vertices covered: " +
-                  str(round((v_counter / v_max) * 100, 2)) + "%")
+            print("Percentage of vertices covered: " +
+                  str(round((v_counter / v_max) * 100, 3)) + "%")
             draw_vertices(v_child, r, h, k, G)
 
 
