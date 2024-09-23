f6be92ee2d74aabc74905701915bce18f721b099
Benjamin Coban <coban@fzi.de>
2024-09-23 17:36:55 +0200

    renaming + figure as a session state

--- demonstrator_template.py
+++ demonstrator_template.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 
 st.title("Python Demonstrator Template")
-st.sidebar.success("This is a template for a Streamlit demonstrator.")
+st.sidebar.success("This is a template for a demonstrator setup using streamlit for a Web App to display your Python prototype.")
--- pages/draw_result.py
+++ pages/draw_result.py
@@ -12,18 +12,21 @@ def main():
     st.session_state['logging'] = st.sidebar.checkbox("Enable / Disable a log of the graph drawn")
     st.session_state['color'] = st.sidebar.checkbox("Enable / Disable coloring of the vertices")
     start_drawing = st.sidebar.button("Draw Tree", on_click=trigger_drawing)
-    
+    if start_drawing:
+        st.sidebar.success("Drawing the tree...")
 
 def trigger_drawing():
-    figure = draw.draw( 
+    st.session_state['figure'] = draw.draw( 
                         st.session_state['k'], 
                         st.session_state['h'], 
                         st.session_state['integer'], 
                         st.session_state['logging'], 
                         st.session_state['color']
                     )
-    st.pyplot(figure)
-    st.write()        
+    st.pyplot(st.session_state['figure'])
+    st.snow()
+    st.success("The tree has been drawn.")
+
 
 if __name__ == "__main__":
     main()
